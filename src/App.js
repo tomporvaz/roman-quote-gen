c8f67a68f347138159467841ca81989e65953eba
Tom Porvaznik <tomporvaz@gmail.com>
2019-01-26 20:25:50 -0500

    changed default react practice app to FCC: Random Quote Machine.  Fixed twitter button to include text of quote in tweet, and added FCC test suite CDN.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "Carpe diem",
+      translation: "Seize the day",
+      author: "Horace"
+    };
+    //add bindings here
+  }
+
   render() {
+    const tweetHREF = "https://twitter.com/intent/tweet?button_hashtag=romanquotes&text=" + 
+    encodeURIComponent('"' + this.state.quote + '" ' + this.state.author);
+    
+    const testHREF = "https://www.google.com/";
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div>
+        <h1 className="header"> Random Quote Generator </h1>
+        <div id="quote-box">
+          <h3 id="text">{this.state.quote}</h3>
+          <p id="translation">{this.state.translation}</p>
+          <p id="author">~{this.state.author}</p>
+          <a  id="tweet-quote" href={tweetHREF} target="_blank">
+            <button >Tweet Quote</button>
           </a>
-        </header>
+          
+          <button id="new-quote" onClick={this.handleClick}>New Quote</button>
+        </div>
       </div>
     );
   }
