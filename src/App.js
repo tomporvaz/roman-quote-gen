914d927cbdaf47242d486ac140bcbfc68c907333
Tom Porvaznik <tomporvaz@gmail.com>
2019-01-27 19:40:50 -0500

    added an JSON of quote objects called quotesJSON.  Updated state to accept a quote object and updated JSX to display the appropirate property of each quote object.  QuoteOBJ in state is currently staticly defined by index number.

--- src/App.js
+++ src/App.js
@@ -2,32 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const quotesJSON = {quotes:[
+  {
+    quote: "Carpe Diem",
+    translation: "Seize the day.",
+    author: "Horace"
+  },
+  {
+    quote: "Tempest Fugit",
+    translation: "Time flies.",
+    author: "Virgil"
+  },
+  {
+    quote: "SEMPER UBI SUB UBI",
+    translation: "Always where under where.",
+    author: "Mr. Talone"
+  }
+
+
+]}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: "Carpe diem",
+      quoteObj: quotesJSON.quotes[0],
+      //quote: quoteObj.quote,
       translation: "Seize the day",
       author: "Horace"
     };
     //add bindings here
+    //this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
     const tweetHREF = "https://twitter.com/intent/tweet?button_hashtag=romanquotes&text=" + 
-    encodeURIComponent('"' + this.state.quote + '" ' + this.state.author);
+    encodeURIComponent('"' + this.state.quoteObj.quote + '" ' + '(' + this.state.quoteObj.translation + ') ~ ' + this.state.author);
     
     const testHREF = "https://www.google.com/";
     
     return (
       <div>
-        <h1 className="header"> Random Quote Generator </h1>
+        <h1 className="header"> Roman Quote Generator </h1>
         <div id="quote-box">
-          <h3 id="text">{this.state.quote}</h3>
-          <p id="translation">{this.state.translation}</p>
-          <p id="author">~{this.state.author}</p>
+          <h3 id="text">{this.state.quoteObj.quote}</h3>
+          <p id="translation">{this.state.quoteObj.translation}</p>
+          <p id="author">~{this.state.quoteObj.author}</p>
           <a  id="tweet-quote" href={tweetHREF} target="_blank">
-            <button >Tweet Quote</button>
+            <button onClick={this.handleClick}>Tweet Quote</button>
           </a>
           
           <button id="new-quote" onClick={this.handleClick}>New Quote</button>
