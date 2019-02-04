e0be9be89deafaf2c47432843e57d4a74ff184ab
Tom Porvaznik <tomporvaz@gmail.com>
2019-02-03 19:33:56 -0500

    Updates

---
+++ precache-manifest.59a0424da97503d851c6b95f71887b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roman-quote-gen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36701c5d6634fabfd54b",
+    "url": "/roman-quote-gen/static/js/runtime~main.36701c5d.js"
+  },
+  {
+    "revision": "2236caca7f3c8134bdd1",
+    "url": "/roman-quote-gen/static/js/main.2236caca.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/roman-quote-gen/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "2236caca7f3c8134bdd1",
+    "url": "/roman-quote-gen/static/css/main.d2f8ad33.chunk.css"
+  },
+  {
+    "revision": "bb0519d843585532da8558c4bf2bca5f",
+    "url": "/roman-quote-gen/index.html"
+  }
+];
